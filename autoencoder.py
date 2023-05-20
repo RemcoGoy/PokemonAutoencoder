906a805e32b25a31f38b2854fa70c7eded8a69ba
RemcoGoy <remco.goy@hotmail.com>
2023-05-20 13:14:44 +0200

    Project setup

---
+++ autoencoder.py
@@ -0,0 +1,7 @@
+import torch
+from torchvision import datasets
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
+# Transforms images to a PyTorch Tensor
+tensor_transform = transforms.ToTensor()
